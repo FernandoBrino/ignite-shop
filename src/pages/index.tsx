c567cf09998b09dafa0347d62fe2c911f17d76fc
Fernando Brino <fernandobrino11@gmail.com>
2023-01-08 16:08:31 -0300

    Product style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,40 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 8,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
-
+import camiseta1 from '../assets/camisetas/camiseta1.png'
+import camiseta2 from '../assets/camisetas/camiseta2.png'
+import camiseta3 from '../assets/camisetas/camiseta3.png'
 
 export default function Home() {
   return (
-      <Button>
-        <span>Teste</span>
-        Enviar
-      </Button>
+      <HomeContainer>
+        <Product>
+          <Image 
+            src={camiseta1} 
+            width={520} 
+            height={480} 
+            alt="" 
+          />
+
+          <footer>
+            <strong>Camiseta 1</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product>
+          <Image 
+            src={camiseta2} 
+            width={520} 
+            height={480} 
+            alt="" 
+          />
+
+          <footer>
+            <strong>Camiseta 2</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
   )
 }
