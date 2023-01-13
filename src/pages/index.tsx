259cc1c989b11a2f27a505d36f088909025fd6fc
Fernando Brino <fernandobrino11@gmail.com>
2023-01-12 22:08:44 -0300

    Fix Image error next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,21 +32,20 @@ export default function Home({ products }: HomeProps) {
       <HomeContainer ref={sliderRef} className="keen-slider">
         {products.map(product => {
           return (
-            <Link href={`/product/${product.id}`} key={product.id}>
-              <Product className="keen-slider__slide" >
-                  <Image 
-                    src={product.imageUrl} 
-                    width={520} 
-                    height={480} 
-                    alt="" 
-                  />
+            <Product href={`/product/${product.id}`} key={product.id} className="keen-slider__slide">
+                <Image 
+                  priority
+                  src={product.imageUrl} 
+                  width={520} 
+                  height={480} 
+                  alt="" 
+                />
 
-                  <footer>
-                    <strong>{product.name}</strong>
-                    <span>{product.price}</span>
-                  </footer>
-                </Product>
-            </Link>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+            </Product>
           )
         })}
       </HomeContainer>
