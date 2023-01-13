ec03e25e41d10c18bc62c4ab4478a6231bf27283
Fernando Brino <fernandobrino11@gmail.com>
2023-01-12 21:50:47 -0300

    Style product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,31 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 
 export default function Product() {
     const { query } = useRouter();
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+       <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,00</span>
+
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Corporis dignissimos explicabo, magnam recusandae ducimus fugiat, 
+                    nesciunt possimus ipsum officia veritatis consectetur culpa quaerat tenetur? 
+                    Nisi cupiditate at maxime debitis perferendis!
+                </p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+       </ProductContainer> 
     )
 }
